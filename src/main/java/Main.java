b00e9bcc504a655667556888e24db0f5a0b16668
Kristina Janovcikova <kjanovcikova@hotmail.com>
2017-02-08 00:09:01 +0100

    First Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,31 @@ public class Main {
     public static void main (String[] args) {
 
         //array to sort
-        int[] arr = { 3, 8, 5, 4, 1, 9, -2 };
+        int[] arr = { 9, -5, 0, 11, 1, -2, 7, 3 };
         int n = arr.length;
 
         insertionSort(arr, n);
         printArray(arr, n);
+        System.out.println();
+
+        selectionSort(arr, n);
+        printArray(arr, n);
     }
 
-    /** perform insertion sort on arr[] */
+    /** print n elements of the array arr */
+    static void printArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    /** INSERTION SORT on int[] arr
+     *
+     * Insertion sort is stable, in-place sorting algorithm that builds the final sorted array
+     * one item at a time. It is not very best in terms of performance but it is more efficient
+     * in practice than most other simple O(n2) algorithms such as selection sort or bubble sort.
+     *
+     * */
     static void insertionSort(int[] arr, int n) {
         // Start from second element (element at index 0 is already sorted)
         for (int i = 1; i < n; i++) {
@@ -28,10 +45,30 @@ public class Main {
         }
     }
 
-    /** Function to print n elements of the array arr */
-    static void printArray(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+    /** SELECTION SORT on int[] arr
+     *
+     * Selection sort is a unstable, in-place sorting algorithm known for its simplicity,
+     * and it has performance advantages over more complicated algorithms in certain situations,
+     * particularly where auxiliary memory is limited.
+     */
+    static void selectionSort(int arr[], int n) {
+        // run (n - 1) times
+        for (int i = 0; i < n - 1; i++) {
+            // find the minimum element in the unsorted subarray[i..n-1] and swap it with arr[i]
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                // if arr[j] element is less, then it is the new minimum
+                if (arr[j] < arr[min]) {
+                    min = j;    // update index of min element
+                }
+            }
+
+            // swap the minimum element in subarray[i..n-1] with arr[i]
+            int minElement = arr[min];
+
+            arr[min] = arr[i];
+            arr[i] = minElement;
         }
     }
 }
