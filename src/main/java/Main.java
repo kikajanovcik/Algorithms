8fa2f26f6430dc1b55ff4fcf4f0f76b11da21905
Kristina Janovcikova <kjanovcikova@hotmail.com>
2017-02-11 14:17:36 +0100

    binary searches added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,18 @@ public class Main {
         int[] arr = { 9, -5, 0, 11, 1, -2, 7, 3 };
         int n = arr.length;
 
-        //Try out algorithms by uncommenting of the following methods (:
+        //Try out algorithms by uncommenting one of the following methods (:
 
+        //SORTING algorithms
         //insertionSort(arr, n);
         //selectionSort(arr, n);
         //bubbleSort(arr, n);
+
+        //BINARY search
+        //System.out.println(binarySearch(arr, n, -5));
+        //System.out.println(recursiveBinarySearch(arr, 0, n-1, 1));
+
+        //PRINT array
         printArray(arr, n);
     }
 
@@ -50,16 +57,16 @@ public class Main {
      * and it has performance advantages over more complicated algorithms in certain situations,
      * particularly where auxiliary memory is limited.
      */
-    static void selectionSort(int arr[], int n) {
+    static void selectionSort(int[] arr, int n) {
         // run (n - 1) times
         for (int i = 0; i < n - 1; i++) {
-            // find the minimum element in the unsorted subarray[i..n-1] and swap it with arr[i]
+            // find the minimum element in the unsorted sub-array[i..n-1] and swap it with arr[i]
             int min = i;
 
             for (int j = i + 1; j < n; j++) {
                 // if arr[j] element is less, then it is the new minimum
                 if (arr[j] < arr[min]) {
-                    min = j;    // update index of min element
+                    min = j; // update index of min element
                 }
             }
 
@@ -79,8 +86,8 @@ public class Main {
      * and is not recommended when n is large.
      */
 
-    static void bubbleSort(int arr[], int n) {
-        // (n - 1) pass
+    static void bubbleSort(int[] arr, int n) {
+        // run (n - 1) times
         for (int j = 0; j < n - 1; j++) {
             // last j items are already sorted, so inner loop can avoid looking at the last j items
             // the algorithm can be stopped if the inner loop didn’t do any swap
@@ -92,6 +99,68 @@ public class Main {
                     arr[i] = next;
                 }
             }
+        }
+    }
+
+
+    /** Binary search
+     *
+     * Reduces the search space to half at each step
+     * Returns the position of target in the arr of size n
+     * Array must be sorted beforehand!
+     * **/
+
+    static int binarySearch(int[] arr, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+
+        // iterate till search space contains at least one element
+        while (low <= high) {
+            // find the mid value in the search space and compare it with target value
+
+            int mid = (low + high) / 2;    // overflow can happen
+            // int mid = low + (high - low)/2;
+            // int mid = high - (high - low)/2;
+
+            if (target == arr[mid]) {
+                return mid; // target value is found, yay!
+
+            // If target < mid, discard all elements in the right search space including the mid element
+            } else if (target < arr[mid]) {
+                high = mid - 1;
+
+            // If target > mid, discard all elements in the left search space including the mid element
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        // target doesn't exist in the array
+        return -1;
+    }
+
+    /** Recursive Binary search
+     *
+     * Returns the position of target in the sub-array arr[low..high]
+     * Array must be sorted beforehand!
+     */
+    static int recursiveBinarySearch(int[] arr, int low, int high, int target) {
+        // Base condition
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + high)/2;    // overflow can happen
+        // int mid = low + (high - low)/2;
+
+        if (target == arr[mid]) {
+            return mid; // target value is found, yay!
+        } else if (target < arr[mid]) {
+            // discard all elements in the right search space, including the mid element
+            return recursiveBinarySearch(arr, low, mid - 1, target);
+        } else {
+            // discard all elements in the left search space, including the mid element
+            return recursiveBinarySearch(arr, mid + 1, high, target);
         }
     }
 }
