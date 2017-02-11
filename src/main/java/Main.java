a686ecad1ded281f4945ad72369c5baff54306d8
Kristina Janovcikova <kjanovcikova@hotmail.com>
2017-02-11 13:44:15 +0100

    bubbleSort() added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,11 @@ public class Main {
         int[] arr = { 9, -5, 0, 11, 1, -2, 7, 3 };
         int n = arr.length;
 
-        insertionSort(arr, n);
-        printArray(arr, n);
-        System.out.println();
+        //Try out algorithms by uncommenting of the following methods (:
 
-        selectionSort(arr, n);
+        //insertionSort(arr, n);
+        //selectionSort(arr, n);
+        //bubbleSort(arr, n);
         printArray(arr, n);
     }
 
@@ -27,8 +27,7 @@ public class Main {
      * Insertion sort is stable, in-place sorting algorithm that builds the final sorted array
      * one item at a time. It is not very best in terms of performance but it is more efficient
      * in practice than most other simple O(n2) algorithms such as selection sort or bubble sort.
-     *
-     * */
+     */
     static void insertionSort(int[] arr, int n) {
         // Start from second element (element at index 0 is already sorted)
         for (int i = 1; i < n; i++) {
@@ -69,6 +68,30 @@ public class Main {
 
             arr[min] = arr[i];
             arr[i] = minElement;
+        }
+    }
+
+    /** BUBBLE SORT on int[] arr
+     *
+     * Bubble sort is a stable, in-place sorting algorithm that is named for the way smaller
+     * or larger elements “bubble” to the top of the list. Although the algorithm is simple,
+     * it is too slow and impractical for most problems even when compared to insertion sort
+     * and is not recommended when n is large.
+     */
+
+    static void bubbleSort(int arr[], int n) {
+        // (n - 1) pass
+        for (int j = 0; j < n - 1; j++) {
+            // last j items are already sorted, so inner loop can avoid looking at the last j items
+            // the algorithm can be stopped if the inner loop didn’t do any swap
+            for (int i = 0; i < n - 1 - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int next = arr[i + 1];
+
+                    arr[i + 1] = arr[i];
+                    arr[i] = next;
+                }
+            }
         }
     }
 }
